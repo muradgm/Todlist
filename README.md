360f097dcf7c1fc44aab4555b9d12be468791e70
muradgm <59296531+muradgm@users.noreply.github.com>
2022-05-12 15:52:34 +0200

    updated the README file

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
-# ToDoList
-
+# Todo List
+###### A Mini React app/ exercise to build a Todo List
+## Demo
+  ![Exercise Demo](./todo-list.gif "Title Text - Todo List")
+## What have i achieved:
 - The user should have the ability to add a todo item through the input field
 - The list of all todo items should be displayed for the user
 - The user should be able to delete a todo item and to mark it as complete
-- Here is an image for reference:
-    ![Mock-up-image](Mock-up-image.png)
+### Modules/Packages/Libraries used:
+```
+    - react: ^17.0.2
+    - react-bootstrap: ^2.2.2
+    - sass: ^1.49.7
+    ```
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,10 +8,8 @@ export default function TodoList(props) {
   const [text, setText] = useState("")
   const [complete, setComplete] = useState(false)
   
-  const clearInput = useRef()
   const handleTextOnChange = (e) => {
-    console.log(e.target.value);
-    setText(e.target.value)
+      setText(e.target.value)
   }
   
   const handleSubmit = (e) => {
@@ -20,23 +18,27 @@ export default function TodoList(props) {
     const updateTodos = [...todos, text]
     setTodos(updateTodos)
     console.log(text);
-    
-    e.target.value = ''
-
+    setText("")
   }
 
     const handleClick = (item) => {
-    const updateTodos = todos.filter(x => x !== item)
-    setTodos(updateTodos)
+      const updateTodos = todos.filter(x => x !== item)
+      setTodos(updateTodos)
     }
   
   // const selectedTask = useRef()
 
-  const handleCompleted = (e) => {
-    console.log(e.target.parentElement);
-    e.target.parentElement.firstChild.style.textDecoration = "line-through"
-    console.log(e.target.parentElement.firstChild);
-    setComplete(true)
+  const handleCompleted = (e, task) => {
+
+    console.log(completedTodo);
+    console.log(e.target)
+    if (complete === false) {
+      setComplete(true)
+      e.target.parentElement.firstChild.style.textDecoration = "line-through"
+    } else {
+      e.target.parentElement.firstChild.style.textDecoration = "none"
+      setComplete(false)
+    }
   }
   
   return (
@@ -47,7 +49,7 @@ export default function TodoList(props) {
         <InputGroup.Text placeholder='Add a new task...'>
           Define your task
         </InputGroup.Text>
-        <FormControl aria-label="Default" ref={clearInput} aria-describedby="inputGroup-sizing-default" value={text} onChange={handleTextOnChange} />
+        <FormControl aria-label="Default" aria-describedby="inputGroup-sizing-default" value={text} onChange={handleTextOnChange} />
         <Button variant="primary" type="submit" onClick={handleSubmit}> add task </Button>
       </InputGroup>
   </Container>
@@ -59,7 +61,7 @@ export default function TodoList(props) {
       <ListGroup.Item as="li" key={i} index={i} className="d-flex bd-highlight example-parent">
         <h5 className="pt-2 flex-grow-1 col-example" >{task}</h5>
 
-          {<Button variant="success"  onClick={handleCompleted } >{!complete ? (<> Pending! </>) : ( <> <CheckLg /> Completed </>)}</Button>}{' '}
+          {<Button variant={complete ? 'success' : 'warning'} onClick={ handleCompleted } >{!complete ? (<> Pending! </>) : ( <> <CheckLg /> Completed </>)}</Button>}{' '}
     
           {<Button variant="outline-danger"  className="ms-2" onClick={() => { handleClick(task) }} data-tip data-for="deletionTip"><XLg /> Delete</Button>}
           <ReactTooltip id="deletionTip" place="top" effect="solid"> Delete Task!
